a9d3d4ea2c854facc0af1dbdfd7c32cdec0fd377
TheSmartHyena <skopal.philippe@gmail.com>
2021-07-04 12:32:08 +0200

    First version of the calculator, ugly, but works. Bootstraped 4.6

---
+++ src/Calc.js
@@ -0,0 +1,91 @@
+import { Button, Container, Row, Col, Card, Form } from 'react-bootstrap';
+import axios from 'axios';
+import { useState } from "react";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Calc.css';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+const Calc = () => {
+
+  const [currentCalc, setCurrentCalc] = useState("");
+  const [previousCalc, setPreviousCalc] = useState("");
+
+  const handleSend = async () => {
+    const result = await axios.post(`http://localhost:8080/calc`, {calculus: currentCalc});
+    setPreviousCalc(currentCalc + " = " + result.data.result);
+    setCurrentCalc(result.data.result);
+  };
+
+  const addChar = (char) => {
+    setCurrentCalc(currentCalc + char);
+  }
+
+  const resetChars = () => {
+    setPreviousCalc("");
+    setCurrentCalc("");
+  }
+
+  const removeLastChar = () => {
+    setCurrentCalc(currentCalc.slice(0, -1));
+  }
+
+  return (
+    <div className="Calc">
+      <Container>
+        <Row>
+          <Col centered>
+            <h1 class="display-4" >Simple calculator</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Card style={{ width: '18rem' }} centered>
+            <Card.Body>
+              <div class="inputs-body">
+                <Card.Text>
+                  <Form.Control type="text" value={previousCalc} readOnly/>
+                  <Form.Control type="text" value={currentCalc} readOnly/>
+                </Card.Text>
+              </div>
+              <div class="buttons-body">
+                <Card.Text>
+                  <Row>
+                    <Col xs={6} ><Button onClick={() => resetChars()} variant="outline-secondary">C</Button>{' '}</Col>
+                    <Col xs={3} ><Button onClick={() => removeLastChar()} variant="outline-secondary"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/></svg></Button>{' '}</Col>
+                    <Col xs={3} ><Button onClick={() => addChar("/")} variant="outline-secondary">/</Button>{' '}</Col>
+                  </Row>
+                  <Row>
+                    <Col xs={3}><Button onClick={() => addChar("7")} variant="outline-secondary">7</Button>{' '}</Col>
+                    <Col xs={3}><Button onClick={() => addChar("8")} variant="outline-secondary">8</Button>{' '}</Col>
+                    <Col xs={3}><Button onClick={() => addChar("9")} variant="outline-secondary">9</Button>{' '}</Col>
+                    <Col xs={3}><Button onClick={() => addChar("*")} variant="outline-secondary"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></Button>{' '}</Col>
+                  </Row>
+                  <Row>
+                    <Col xs={3}><Button onClick={() => addChar("4")} variant="outline-secondary">4</Button>{' '}</Col>
+                    <Col xs={3}><Button onClick={() => addChar("5")} variant="outline-secondary">5</Button>{' '}</Col>
+                    <Col xs={3}><Button onClick={() => addChar("6")} variant="outline-secondary">6</Button>{' '}</Col>
+                    <Col xs={3}><Button onClick={() => addChar("-")} variant="outline-secondary">-</Button>{' '}</Col>
+                  </Row>
+                  <Row>
+                    <Col xs={3}><Button onClick={() => addChar("1")} variant="outline-secondary">1</Button>{' '}</Col>
+                    <Col xs={3}><Button onClick={() => addChar("2")} variant="outline-secondary">2</Button>{' '}</Col>
+                    <Col xs={3}><Button onClick={() => addChar("3")} variant="outline-secondary">3</Button>{' '}</Col>
+                    <Col xs={3}><Button onClick={() => addChar("+")} variant="outline-secondary">+</Button>{' '}</Col>
+                  </Row>
+                  <Row>
+                    <Col xs={6}><Button onClick={() => addChar("0")} variant="outline-secondary">0</Button>{' '}</Col>
+                    <Col xs={3}><Button onClick={() => addChar(".")} variant="outline-secondary">.</Button>{' '}</Col>
+                    <Col xs={3}><Button onClick={() => handleSend()} variant="outline-secondary">=</Button>{' '}</Col>
+                  </Row>                    
+                </Card.Text>     
+              </div>            
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
+
+    </div>
+  );
+}
+
+export default Calc;
--- src/Calct.test.js
+++ src/Calct.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Calc from './Calc';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Calc />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
